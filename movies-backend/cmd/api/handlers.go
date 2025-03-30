d1f7d3c8f9d226c88553a901107f718a3406aca6
Mamun172136 <ma1536204@gmail.com>
2025-03-30 20:56:38 +0600

    added write json

--- movies-backend/cmd/api/handlers.go
+++ movies-backend/cmd/api/handlers.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func (app *application)Home(w http.ResponseWriter, r *http.Request) {
@@ -30,27 +28,34 @@ func (app *application)Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request){
-	var movies []models.Movie
-	rd, _ := time.Parse("2006-01-02", "1986-03-07")
-	highlander := models.Movie{
-		ID:1,
-		Title: "Highlander",
-		ReleaseDate:rd ,
-		MPAARating: "R",
-		Runtime: 116,
-		Description: "nice movie",
-		CreatedAt:time.Now() ,
-		UpdatedAt: time.Now(),
-	}
+	// var movies []models.Movie
+	// rd, _ := time.Parse("2006-01-02", "1986-03-07")
+	// highlander := models.Movie{
+	// 	ID:1,
+	// 	Title: "Highlander",
+	// 	ReleaseDate:rd ,
+	// 	MPAARating: "R",
+	// 	Runtime: 116,
+	// 	Description: "nice movie",
+	// 	CreatedAt:time.Now() ,
+	// 	UpdatedAt: time.Now(),
+	// }
 
-	movies = append(movies, highlander)
+	// movies = append(movies, highlander)
 
-	out, err := json.Marshal(movies)
+	movies, err :=  app.DB.AllMovies()
 	if err != nil{
 		fmt.Println(err)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	// out, err := json.Marshal(movies)
+	// if err != nil{
+	// 	fmt.Println(err)
+	// }
+
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(http.StatusOK)
+	// w.Write(out)
+
+	_ =  app.writeJSON(w, http.StatusOK, movies)
 }
