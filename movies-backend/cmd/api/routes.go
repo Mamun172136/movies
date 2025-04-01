32acae14e930ebb9ea8bf2edd3ff9dbf52460666
Mamun172136 <ma1536204@gmail.com>
2025-04-02 04:13:05 +0600

    added header verify

--- movies-backend/cmd/api/routes.go
+++ movies-backend/cmd/api/routes.go
@@ -11,8 +11,14 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Get("/",app.Home)
+	mux.Get("/movies", app.AllMovies)
 	mux.Post("/authenticate", app.authenticate)
 	mux.Get("/refresh", app.refreshToken)
 	mux.Get("/logout", app.logout)
+	mux.Route("/admin", func(mux chi.Router){
+		mux.Use(app.authRequired)
+
+		mux.Get("/movies", app.MovieCatalog)
+	})
 	return mux
 }
