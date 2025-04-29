b19e8e130fa49f8839795179ee224adfa45b1344
Mamun172136 <ma1536204@gmail.com>
2025-04-30 04:49:33 +0600

    update db url

--- movies-backend/cmd/api/main.go
+++ movies-backend/cmd/api/main.go
@@ -28,11 +28,13 @@ func main() {
 	var app application
 
 	//read from command line
-	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	// flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.DSN, "dsn", "postgresql://postgres:JwcgamlQaDuEPPatzVzijvuVEAJtUHdf@containers-us-west-45.railway.app:5432/railway", "Postgres connection string")
 	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
 	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
 	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
-	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "postgres-production-7120.up.railway.app", "cookie domain")
+	flag.StringVar(&app.APIKey, "api-key", "b41447e6319d1cd467306735632ba733", "api key")
 	flag.Parse()
 	//connect to database
 	conn , err:= app.connectToDB()
